330a5c52bd505dcad3652c6518da45ae2cd7cbee
Robbie McKinstry <r.mckinstry@modcloth.com>
2014-08-16 17:58:53 -0400

    Added another test.

--- spec/test_obj_initialization.py
+++ spec/test_obj_initialization.py
@@ -35,6 +35,31 @@ class TestObjectInitialization(unittest.TestCase):
         
         expected = add_a_game()
         observed = query_for_game()
+        session.close()
+        assert expected is observed, 'Observed:\t{0}\nExpected:\t{1}'.format(observed, expected)
+
+    def test_when_game_stats_is_made(self):
+        session = SessionMaker()
+        def add_a_game_stats():
+            game_stats = GameStats(
+                summoner_name = 'Bjerson',
+                champion = 'Garen',
+                spell1 = 'Ignite',
+                spell2 = 'Flash',
+                item0 =  'Bork',
+                blue = True,
+                won = True,
+            )
+            session.add(game_stats)
+            session.commit()
+            return game_stats
+
+        def query_for_game_stats():
+            return session.query(GameStats).filter_by(summoner_name='Bjerson').first()
+
+        expected = add_a_game_stats()
+        observed = query_for_game_stats()
+        session.close()
         assert expected is observed, 'Observed:\t{0}\nExpected:\t{1}'.format(observed, expected)
 
 if __name__ == '__main__':
