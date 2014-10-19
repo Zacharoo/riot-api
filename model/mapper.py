4f17da3049f73f06951d95c04dd1e76afc0d1be8
Robbie McKinstry <bravenewprince@gmail.com>
2014-10-18 21:50:44 -0400

    Refactored to expose internal functions

--- model/mapper.py
+++ model/mapper.py
@@ -2,35 +2,38 @@
 from model.game import GameStats
 from api.riot_api import *
 
+def store_game_stats_in_db(game, session):
+    session.add(game)
+    session.commit()
+    session.close()
+    return game
+
+
+def make_game_stats_from_game(game):
+    champ_name = get_champ_name_from_game(game)
+    items = get_item_names_by_game(game)
+    win = get_win_from_game(game) 
+    blue = get_blue_from_game(game)
+    summoner_name = name
+
+    game_stats = GameStats(
+        summoner_name = summoner_name,
+        champion = champ_name,
+        item0 = items[0],
+        item1 = items[1],
+        item2 = items[2],
+        item3 = items[3],
+        item4 = items[4],
+        item5 = items[5],
+        item6 = items[6],
+        won = win,
+        blue = blue,
+    )
+    return game_stats
+
+
 def fetch_and_store_summoner(name, db):
-    def make_game_stats_from_game(game):
-        champ_name = get_champ_name_from_game(game)
-        items = get_item_names_by_game(game)
-        win = get_win_from_game(game) 
-        blue = get_blue_from_game(game)
-        summoner_name = name
-
-        game_stats = GameStats(
-            summoner_name = summoner_name,
-            champion = champ_name,
-            item0 = items[0],
-            item1 = items[1],
-            item2 = items[2],
-            item3 = items[3],
-            item4 = items[4],
-            item5 = items[5],
-            item6 = items[6],
-            won = win,
-            blue = blue,
-        )
-        return game_stats
-
-    def store_game_stats_in_db(game, session):
-        session.add(game)
-        session.commit()
-        session.close()
-        return game
-        
+    
     summoner = get_summoner_by_name(name)
     games = get_games_by_summoner(summoner)
  
